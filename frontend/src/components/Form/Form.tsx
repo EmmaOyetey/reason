8c2ad48ce871a28050928d559cdf2366d61fc073
Emma Oyetey <emmaflookis@hotmail.com>
2024-05-15 22:11:59 +0100

    created form component to capture new user

--- frontend/src/components/Form/Form.tsx
+++ frontend/src/components/Form/Form.tsx
@@ -1,76 +1,63 @@
 import { ChangeEvent, FormEvent, useState } from "react";
 import "./Form.scss";
-//import QuestionRequest from "../../types/QuestionRequest";
+import UserRequest from "../../types/UserRequest";
 import OptionType from "../../types/OptionType";
 import Select from "../../components/Select/select";
 
-/* 
-  Form styles borrowed from: 
-  https://codepen.io/banunn/pen/AFnal
-*/
 
 type FormProps = {
-  defaultFormState: QuestionRequest;
+  defaultFormState: UserRequest;
   formTitle: string;
-  handleSubmit: (question: QuestionRequest) => void;
-  users: OptionType[];
-  difficulty_rating: OptionType[];
+  handleSubmit: (user: UserRequest) => void;
+  schools: OptionType[];
 };
 
-const Form = ({ defaultFormState, handleSubmit, formTitle, users, countries }: FormProps) => {
-  const [greeting, setGreeting] = useState<GreetingRequest>(defaultFormState);
+const Form = ({ defaultFormState, handleSubmit, formTitle, schools }: FormProps) => {
+  const [user, setUser] = useState<UserRequest>(defaultFormState);
 
   const handleValidation = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    if (Object.values(greeting).some(value => !value)) {
+    if (Object.values(user).some(value => !value)) {
       alert("Missing content, unable to proceed");
       return;
     }
 
-    handleSubmit(greeting);
+    handleSubmit(user);
   };
 
   const handleInput = (event: FormEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>, key: string) =>
-    setGreeting({ ...greeting, [key]: event.currentTarget.value });
+    setUser({ ...user, [key]: event.currentTarget.value });
 
   return (
     <div className="form-container">
       <h2 className="form-container__title">{formTitle}</h2>
       <form className="form-container__form" onSubmit={handleValidation}>
         <Select
-          labelText={"Select User : "}
-          label={"userId"}
-          options={users}
-          onChange={event => handleInput(event, "userId")}
+          labelText={"Select School : "}
+          label={"schoolId"}
+          options={schools}
+          onChange={event => handleInput(event, "schoolId")}
           defaultOption={"---"}
-          defaultValue={greeting.userId.toString()}
+          defaultValue={user.schoolId.toString()}
         />
-        <Select
-          labelText={"Select Country : "}
-          label={"countryId"}
-          options={countries}
-          onChange={event => handleInput(event, "countryId")}
-          defaultOption={"---"}
-          defaultValue={greeting.countryId.toString()}
-        />
-        <label htmlFor="greeting">Greeting : </label>
+        <label htmlFor="First name">First Name : </label>
         <input
-          id="greeting"
+          id="first_name"
           className="form-container__input"
           type="text"
-          placeholder="greeting"
-          value={greeting.greeting}
-          onInput={event => handleInput(event, "greeting")}
+          placeholder="First Name"
+          value={user.firstName}
+          onInput={event => handleInput(event, "firstName")}
         />
-        <label htmlFor="nationality">Nationality : </label>
+        <label htmlFor="user name">User Name : </label>
         <input
-          id="nationality"
+          id="user_name"
           className="form-container__input"
           type="text"
-          placeholder="nationality"
-          value={greeting.nationality}
-          onInput={event => handleInput(event, "nationality")}
+          placeholder="User Name"
+          value={user.userName}
+          onInput={event => handleInput(event, "userName")}
         />
         <button type="submit" className="form-container__button">
           Submit
---
+++ frontend/src/types/UserRequest.ts
@@ -0,0 +1,9 @@
+type UserRequest = {
+    id: number;
+    firstName: string;
+    userName: string;
+    schoolId: number;
+    schoolYear: number;
+  };
+  
+  export default UserRequest;
